dac8e642ed0048574e7355bcb974a49a6257a0f9
Hrynchuk Taras <hrynchuktt@gmail.com>
2024-03-18 16:48:51 +0200

    ui libraries & types config

--- src/app/appEntry.tsx
+++ src/app/appEntry.tsx
@@ -2,13 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "@/app/styles/index.css";
 import { Provider } from "react-redux";
+import { NextUIProvider } from "@nextui-org/react";
 import { BaseLayout } from "./layouts/BaseLayout.tsx";
 import { store } from "./appStore.ts";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
    <React.StrictMode>
       <Provider store={store}>
-         <BaseLayout />
+         <NextUIProvider>
+            <BaseLayout />
+         </NextUIProvider>
       </Provider>
    </React.StrictMode>
 );
