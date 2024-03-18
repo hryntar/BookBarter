78308c1f5e8a46eb12b647e6ddf4a8b7f53d4779
Hrynchuk Taras <hrynchuktt@gmail.com>
2024-03-18 16:55:05 +0200

    hooks & persist login

--- src/app/layouts/BaseLayout.tsx
+++ src/app/layouts/BaseLayout.tsx
@@ -1,9 +1,9 @@
+import { Outlet } from "react-router-dom";
+
 export const BaseLayout = () => {
    return (
-      <>
-         <h1>BookBarter</h1>
-         {/* <Header /> */}
-         {/* <Main /> */}
-      </>
+      <main className="h-[100vh] flex items-center justify-center px-3.5">
+         <Outlet /> 
+      </main>
    );
 };
