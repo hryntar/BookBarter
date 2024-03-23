216ea0f94552111da3d22414cf406b88e2e050f8
Hrynchuk Taras <hrynchuktt@gmail.com>
2024-03-23 19:04:48 +0200

    image upload custom hook

--- src/shared/index.ts
+++ src/shared/index.ts
@@ -1,5 +1,6 @@
 import useLocalStorage from "./hooks/useLocalStorage";
 import useToggle from "./hooks/useToggle";
 import useInput from "./hooks/useInput";
+import useImageUpload from "./hooks/useImageUpload";
 
-export { useToggle, useLocalStorage, useInput };
+export { useToggle, useLocalStorage, useInput, useImageUpload };
