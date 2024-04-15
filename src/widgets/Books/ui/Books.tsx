339725f50ece1792b8133e405bc907c9c7a3c14e
Hrynchuk Taras <hrynchuktt@gmail.com>
2024-04-15 16:43:59 +0300

    adaptive layout fixes

--- src/widgets/Books/ui/Books.tsx
+++ src/widgets/Books/ui/Books.tsx
@@ -8,11 +8,13 @@ export const Books = () => {
    return isLoading ? (
       <Spinner size="lg" className="absolute translate-x-[-50%] translate-y-[-50%] left-[50%] top-[50%]" color="primary"></Spinner>
    ) : (
-      <section className="mt-[80px] px-10 mx-auto">
-         <div className="grid grid-cols-3 gap-y-10">
-            {data?.map((book) => (
-               <BookItem key={book.id} {...book} />
-            ))}
+      <section className="mt-[80px] max-sm:mt-[20px] sm:px-10 mx-auto">
+         <div className="flex flex-col items-center">
+            <div className="grid grid-cols-3 gap-10 max-2xl:grid-cols-2 max-lg:grid-cols-1">
+               {data?.map((book) => (
+                  <BookItem key={book.id} {...book} />
+               ))}
+            </div>
          </div>
       </section>
    );
